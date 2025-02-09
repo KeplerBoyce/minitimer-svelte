554c1fdc8840993e7902ba0bae81a6a0f5eefb49
KeplerBoyce <keplerboy@gmail.com>
2025-02-09 18:10:30 -0500

    responsive timer positioning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,15 @@
 </script>
 
 <div class="p-4 pl-0 w-full h-full flex">
-  <Sidebar />
-  <div class="grow h-full flex flex-col justify-center items-center">
+  <div class="w-72 bg-purple-200">
+    <Sidebar />
+  </div>
+  <div class="grow min-w-min lg:min-w-[735px] h-full bg-blue-200 flex flex-col justify-center items-center">
     <Scramble />
     <div class="grow flex flex-col justify-center items-center">
       <Timer />
       <TimerOptions />
     </div>
   </div>
+  <div class="w-0 lg:w-72 shrink"></div>
 </div>
