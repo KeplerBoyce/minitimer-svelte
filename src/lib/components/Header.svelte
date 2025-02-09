cf83488585ad898e3e4a70206fb8bc571641674f
KeplerBoyce <keplerboy@gmail.com>
2025-02-09 12:39:28 -0500

    Fix header height shrinking

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<div class="w-full h-16 flex justify-center items-center border-b border-black shadow-md">
+<div class="w-full min-h-12 flex justify-center items-center border-b border-black shadow-md">
   <h1 class="font-bold text-3xl">
     MiniTimer
   </h1>
